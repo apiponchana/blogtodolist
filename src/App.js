7a4caf49eda0e873169982850b67e87172629844
apiponchana <apipon.chana@gmail.com>
2023-06-09 21:09:39 +0700

    add new App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./pages/Root";
+import HomePage from "./pages/Home";
+import BlogPage, { loader as blogLoader } from "./pages/Blog";
+import TodoPage, { loader as todoLoader } from "./pages/Todo";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "blog",
+        children: [
+          {
+            index: true,
+            element: <BlogPage />,
+            loader: blogLoader,
+          },
+          {
+            path: ":id",
+            element: <TodoPage />,
+            loader: todoLoader,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
